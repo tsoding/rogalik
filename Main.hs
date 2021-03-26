cbc46abeaf16cc8524c7278334d47fd534a14d62
rexim <reximkut@gmail.com>
2021-03-26 23:21:59 +0700

    Implement custom unlessM

--- Main.hs
+++ Main.hs
@@ -23,28 +23,34 @@ renderRogalik = do
   (_, display) <- runStateT (displayRogalik rogalik) stdDisplay
   lift $ putStrLn $ renderDisplay display
 
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM conditionM body = do
+  condition <- conditionM
+  unless condition body
+
 gameLoop :: StateT Rogalik IO ()
-gameLoop = do
-  lift $ putStr "> "
-  lift $ hFlush stdout
-  line <- lift $ getLine
-  case line of
-    "j" -> do
-      rogalikMove D
-      renderRogalik
-    "k" -> do
-      rogalikMove U
-      renderRogalik
-    "h" -> do
-      rogalikMove L
-      renderRogalik
-    "l" -> do
-      rogalikMove R
-      renderRogalik
-    "q" -> quitRogalik
-    "help" -> lift $ printf "Use vim keybindings to navigate loooool\n"
-    _ -> lift $printf "Unknown command: %s\n" line
-  gameLoop
+gameLoop =
+  unlessM (rogalikQuit <$> getState) $ do
+    lift $ putStr "> "
+    lift $ hFlush stdout
+    line <- lift $ getLine
+    case line of
+      "j" -> do
+        rogalikMove D
+        renderRogalik
+      "k" -> do
+        rogalikMove U
+        renderRogalik
+      "h" -> do
+        rogalikMove L
+        renderRogalik
+      "l" -> do
+        rogalikMove R
+        renderRogalik
+      "q" -> quitRogalik
+      "help" -> lift $ printf "Use vim keybindings to navigate loooool\n"
+      _ -> lift $printf "Unknown command: %s\n" line
+    gameLoop
 
 main :: IO ()
 main = void $ runStateT (renderRogalik >> gameLoop) generateRogalik
