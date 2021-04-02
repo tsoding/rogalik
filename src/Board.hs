6fa552b97c33142ade2b71da7a6f439be4c3d568
rexim <reximkut@gmail.com>
2021-04-03 01:55:20 +0700

    Refactoring
    - Cell -> Point
    - FloorCell -> Cell

--- src/Board.hs
+++ src/Board.hs
@@ -6,46 +6,46 @@ import Data.Array
 import Data.List
 import Data.Function
 
-data Cell = Cell
+data Point = Point
   { cellRow :: Int
   , cellCol :: Int
   } deriving (Eq, Ord, Ix, Show)
 
-liftCell2 :: (Int -> Int -> Int) -> Cell -> Cell -> Cell
-liftCell2 op (Cell row1 col1) (Cell row2 col2) = Cell (row1 `op` row2) (col1 `op` col2)
+liftPoint2 :: (Int -> Int -> Int) -> Point -> Point -> Point
+liftPoint2 op (Point row1 col1) (Point row2 col2) = Point (row1 `op` row2) (col1 `op` col2)
 
-(^+^) :: Cell -> Cell -> Cell
-(^+^) = liftCell2 (+)
+(^+^) :: Point -> Point -> Point
+(^+^) = liftPoint2 (+)
 
-(^-^) :: Cell -> Cell -> Cell
-(^-^) = liftCell2 (-)
+(^-^) :: Point -> Point -> Point
+(^-^) = liftPoint2 (-)
 
 data Rect = Rect
-  { rectCell1 :: Cell
-  , rectCell2 :: Cell
+  { rectPoint1 :: Point
+  , rectPoint2 :: Point
   } deriving (Show)
 
 boardRect :: Board a -> Rect
 boardRect board = uncurry Rect $ bounds $ boardArray board
 
 shrinkRect :: Int -> Rect -> Rect
-shrinkRect s (Rect cell1 cell2) = Rect (cell1 ^+^ Cell s s) (cell2 ^-^ Cell s s)
+shrinkRect s (Rect cell1 cell2) = Rect (cell1 ^+^ Point s s) (cell2 ^-^ Point s s)
 
-clampRect :: Rect -> Cell -> Cell
-clampRect (Rect (Cell row1 col1) (Cell row2 col2)) (Cell row col) =
-  Cell (clamp row row1 row2) (clamp col col1 col2)
+clampRect :: Rect -> Point -> Point
+clampRect (Rect (Point row1 col1) (Point row2 col2)) (Point row col) =
+  Point (clamp row row1 row2) (clamp col col1 col2)
   where
     clamp x l h = min (max x l) h
 
 data Board a = Board
-  { boardArray :: Array Cell a
+  { boardArray :: Array Point a
   } deriving (Show, Functor)
 
-(^!^) :: Board a -> Cell -> a
-(^!^) board cell = boardArray board ! wrapCell board cell
+(^!^) :: Board a -> Point -> a
+(^!^) board cell = boardArray board ! wrapPoint board cell
 
-wrapCell :: Board a -> Cell -> Cell
-wrapCell board cell = liftCell2 mod (cell ^-^ offset) size ^+^ offset
+wrapPoint :: Board a -> Point -> Point
+wrapPoint board cell = liftPoint2 mod (cell ^-^ offset) size ^+^ offset
   where (offset, t) = bounds $ boardArray board
         size = t ^-^ offset
 
@@ -53,19 +53,19 @@ mkBoard :: Int -> Int -> a -> Board a
 mkBoard width height a =
   Board $ array cellRange $ zip (range cellRange) (cycle [a])
   where
-    cellRange = (Cell 1 1, Cell height width)
+    cellRange = (Point 1 1, Point height width)
 
 boardToLists :: Board a -> [[a]]
 boardToLists (Board pixels) =
   map (map snd) $ groupBy ((==) `on` (cellRow . fst)) $ assocs pixels
 
-fillCell :: Cell -> a -> Board a -> Board a
-fillCell cell = fillRect (Rect cell cell)
+fillPoint :: Point -> a -> Board a -> Board a
+fillPoint cell = fillRect (Rect cell cell)
 
 fillRect :: Rect -> a -> Board a -> Board a
 fillRect (Rect cell1 cell2) a board = board {boardArray = pixels // patch}
   where pixels = boardArray board
-        patch = zip (map (wrapCell board) $ range (cell1, cell2)) (cycle [a])
+        patch = zip (map (wrapPoint board) $ range (cell1, cell2)) (cycle [a])
 
 fillBoard :: a -> Board a -> Board a
 fillBoard a board = fillRect (boardRect board) a board
--- src/Rogalik.hs
+++ src/Rogalik.hs
@@ -19,21 +19,21 @@ data Dir
   | D
   deriving (Eq, Ord, Show)
 
-dirV2 :: Dir -> Cell
-dirV2 L = Cell 0 (-1)
-dirV2 R = Cell 0 1
-dirV2 U = Cell (-1) 0
-dirV2 D = Cell 1 0
+dirV2 :: Dir -> Point
+dirV2 L = Point 0 (-1)
+dirV2 R = Point 0 1
+dirV2 U = Point (-1) 0
+dirV2 D = Point 1 0
 
 data Room = Room
   { roomRect :: Rect
-  , roomItems :: M.Map Cell Item
+  , roomItems :: M.Map Point Item
   } deriving (Show)
 
 mkRoom :: Rect -> Room
 mkRoom rect = Room {roomRect = rect, roomItems = M.empty}
 
-addItem :: Cell -> Item -> Room -> Room
+addItem :: Point -> Item -> Room -> Room
 addItem cell item room = room {roomItems = M.insert cell item items}
   where
     items = roomItems room
@@ -41,48 +41,48 @@ addItem cell item room = room {roomItems = M.insert cell item items}
 displayRoom :: Monad m => Room -> StateT (Board Char) m ()
 displayRoom room = do
   modify $ fillRect (roomRect room) '.'
-  let roomPos = rectCell1 $ roomRect room
+  let roomPos = rectPoint1 $ roomRect room
   for_ (M.toList $ roomItems room) $ \(itemPos, item) ->
-    modify $ fillCell (roomPos ^+^ itemPos) (itemChar item)
+    modify $ fillPoint (roomPos ^+^ itemPos) (itemChar item)
 
 data Player = Player
-  { playerPos :: Cell
+  { playerPos :: Point
   , playerGold :: Int
   , playerWeapons :: [Weapon]
   } deriving (Show)
 
-data FloorCell
+data Cell
   = Empty
-  | RoomFloor
+  | Floor
   | VertWall
   | HorzWall
   | Passage
   | Door
   deriving (Show)
 
-floorCellToChar :: FloorCell -> Char
-floorCellToChar Empty = ' '
-floorCellToChar RoomFloor = '.'
-floorCellToChar VertWall = '|'
-floorCellToChar HorzWall = '-'
-floorCellToChar Passage = '#'
-floorCellToChar Door = '+'
+cellToChar :: Cell -> Char
+cellToChar Empty = ' '
+cellToChar Floor = '.'
+cellToChar VertWall = '|'
+cellToChar HorzWall = '-'
+cellToChar Passage = '#'
+cellToChar Door = '+'
 
-floorCellWalkable :: FloorCell -> Bool
-floorCellWalkable Empty = False
-floorCellWalkable RoomFloor = True
-floorCellWalkable VertWall = False
-floorCellWalkable HorzWall = False
-floorCellWalkable Passage = True
-floorCellWalkable Door = True
+cellWalkable :: Cell -> Bool
+cellWalkable Empty = False
+cellWalkable Floor = True
+cellWalkable VertWall = False
+cellWalkable HorzWall = False
+cellWalkable Passage = True
+cellWalkable Door = True
 
 data Rogalik = Rogalik
-  { rogalikBoard :: Board FloorCell
-  , rogalikPlayerPos :: Cell
+  { rogalikBoard :: Board Cell
+  , rogalikPlayerPos :: Point
   , rogalikQuit :: Bool
   } deriving (Show)
 
-rogalikUpdateBoard :: Monad m => StateT (Board FloorCell) m () -> StateT Rogalik m ()
+rogalikUpdateBoard :: Monad m => StateT (Board Cell) m () -> StateT Rogalik m ()
 rogalikUpdateBoard boardState = StateT $ \rogalik -> do
   board' <- execStateT boardState $ rogalikBoard rogalik
   return ((), rogalik { rogalikBoard = board' })
@@ -94,34 +94,34 @@ emptyRogalik :: Int -> Int -> Rogalik
 emptyRogalik width height =
   Rogalik
     { rogalikBoard = board
-    , rogalikPlayerPos = (fst $ bounds $ boardArray board) ^+^ Cell 1 1
+    , rogalikPlayerPos = (fst $ bounds $ boardArray board) ^+^ Point 1 1
     , rogalikQuit = False
     }
   where
     board = mkBoard width height Empty
 
 generateRoomRect :: Monad m => Rect -> StateT Rogalik m ()
-generateRoomRect rect@(Rect (Cell row1 col1) (Cell row2 col2)) = rogalikUpdateBoard $ do
-  modify $ fillRect (Rect (Cell row1 col1) (Cell row2 col1)) VertWall
-  modify $ fillRect (Rect (Cell row1 col2) (Cell row2 col2)) VertWall
-  modify $ fillRect (Rect (Cell row1 col1) (Cell row1 col2)) HorzWall
-  modify $ fillRect (Rect (Cell row2 col1) (Cell row2 col2)) HorzWall
-  modify $ fillRect (shrinkRect 1 rect) RoomFloor
+generateRoomRect rect@(Rect (Point row1 col1) (Point row2 col2)) = rogalikUpdateBoard $ do
+  modify $ fillRect (Rect (Point row1 col1) (Point row2 col1)) VertWall
+  modify $ fillRect (Rect (Point row1 col2) (Point row2 col2)) VertWall
+  modify $ fillRect (Rect (Point row1 col1) (Point row1 col2)) HorzWall
+  modify $ fillRect (Rect (Point row2 col1) (Point row2 col2)) HorzWall
+  modify $ fillRect (shrinkRect 1 rect) Floor
 
-generateRoomAt :: Monad m => Cell -> Int -> Int -> StateT Rogalik m ()
-generateRoomAt pos rows cols = generateRoomRect (Rect pos (pos ^+^ Cell (rows - 1) (cols - 1)))
+generateRoomAt :: Monad m => Point -> Int -> Int -> StateT Rogalik m ()
+generateRoomAt pos rows cols = generateRoomRect (Rect pos (pos ^+^ Point (rows - 1) (cols - 1)))
 
-randomCell :: Rect -> IO Cell
-randomCell (Rect (Cell row1 col1) (Cell row2 col2)) = do
+randomPoint :: Rect -> IO Point
+randomPoint (Rect (Point row1 col1) (Point row2 col2)) = do
   row <- randomRIO (row1, row2)
   col <- randomRIO (col1, col2)
-  return $ Cell row col
+  return $ Point row col
 
 generateRooms :: StateT Rogalik IO ()
 generateRooms =
   replicateM_ 2 $ do
     board <- rogalikBoard <$> get
-    cell <- lift $ randomCell $ boardRect board
+    cell <- lift $ randomPoint $ boardRect board
     w <- lift $ randomRIO (3, 7)
     h <- lift $ randomRIO (3, 7)
     generateRoomAt cell w h
@@ -129,7 +129,7 @@ generateRooms =
 placePlayer :: StateT Rogalik IO ()
 placePlayer = do
   board <- rogalikBoard <$> get
-  let walkables = filter (floorCellWalkable . snd) $ assocs $ boardArray board
+  let walkables = filter (cellWalkable . snd) $ assocs $ boardArray board
   let n = length walkables
   when (n == 0) $ error "Could not find any walkable cells for the player"
   i <- lift $ randomRIO (0, n - 1)
@@ -142,9 +142,9 @@ generateRogalik = do
 
 rogalikMove :: Monad m => Dir -> StateT Rogalik m ()
 rogalikMove dir = modify $ \rogalik ->
-  let playerPos' = rogalikPlayerPos rogalik^+^ dirV2 dir
+  let playerPos' = rogalikPlayerPos rogalik ^+^ dirV2 dir
       board = rogalikBoard rogalik
-   in if floorCellWalkable $ board ^!^ playerPos'
+   in if cellWalkable $ board ^!^ playerPos'
       then rogalik {rogalikPlayerPos = playerPos'}
       else rogalik
 
@@ -152,5 +152,5 @@ renderRogalik :: Rogalik -> [String]
 renderRogalik rogalik =
   boardToLists $
   runIdentity $
-  execStateT (do modify $ fillCell (rogalikPlayerPos rogalik) '@') $
-  floorCellToChar <$> rogalikBoard rogalik
+  execStateT (do modify $ fillPoint (rogalikPlayerPos rogalik) '@') $
+  cellToChar <$> rogalikBoard rogalik
